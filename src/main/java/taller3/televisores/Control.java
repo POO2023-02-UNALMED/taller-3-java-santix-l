bcce9ec723862d76dca10a6813477d57dc64f1bc
santix-l <137665289+santix-l@users.noreply.github.com>
2023-08-26 16:13:43 -0500

    prueba 1

---
+++ src/main/java/taller3/televisores/Control.java
@@ -0,0 +1,47 @@
+package taller3.televisores;
+
+public class Control {
+	
+	private TV tv;
+	
+	public void turnOn() {
+		tv.turnOn();
+	}
+	
+	public void turnOff() {
+		tv.turnOff();
+	}
+	
+	public void canalUp() {
+		
+		tv.canalUp();
+	}
+	
+	public void canalDown() {
+		
+		tv.canalDown();
+	}
+	
+	
+	public void volumenUp() {
+		
+		tv.volumenUp();
+	}
+	
+	public void volumenDown() {
+		
+		tv.volumenDown();
+	}
+	
+	public void setVolumen(int can) {tv.setVolumen(can);}
+	
+	public void setCanal(int can) {tv.setCanal(can);}
+	
+	public void enlazar(TV tv) {
+		
+		this.tv = tv;
+		
+		tv.setControl(this);
+	}
+
+}
---
+++ src/test/java/taller3/televisores/Marca.java
@@ -0,0 +1,24 @@
+package taller3.televisores;
+
+public class Marca {
+	
+	public String nombre;
+	
+	public Marca(String nombre) {
+		
+		this.nombre = nombre;
+		
+	}
+		
+	public void setNombre(String nom) {
+
+		nombre = nom;
+		
+	}
+	
+	public String getNombre() {
+		
+		return nombre;
+	}
+
+}
